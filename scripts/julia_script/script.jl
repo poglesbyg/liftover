ee11524e9f8f497e48e0a37970698892673a2563
Paul Grant <paul_grant@med.unc.edu>
2019-09-20 15:04:57 -0400

    haven't done anything smart for awhile

--- scripts/julia_script/script.jl
+++ scripts/julia_script/script.jl
@@ -1,8 +1,6 @@
 import Pkg
-Pkg.add("Bio")
-using Bio
-using Bio.Seq
-using Bio.Align 
+Pkg.add("CSVFiles")
+using Bio, Bio.Seq, Bio.Align, DelimitedFiles
 hg38 = open(FASTA.Reader, "/proj/ncgenes2/src/ncgenes2-exome-pipeline/modules/apps/human-genome-for-alignment/1405.15/GRCh38_no_alt_analysis_set.refseqids.fna")
 hg37 = open(FASTA.Reader, "/proj/ncgenes2/src/ncgenes2-exome-pipeline/modules/apps/human-genome-for-alignment/grch37/Homo_sapiens.GRCh37.dna_sm.primary_assembly.refseqids.fa")
 
@@ -16,8 +14,8 @@ scoremodel = AffineGapScoreModel(
 )
 
 # data file that holds the lift over data gotten from Crossmap.py
-using CSV
-data = CSV.File("./data.csv")
+# using CSV
+# data = CSV.File("./data.csv")
 
 # for i in data
 # 	hg38 = i.end - i.start
@@ -25,12 +23,3 @@ data = CSV.File("./data.csv")
 # 	print("difference between 38 and 37 is: ")
 # 	println(hg38 - hg37)
 # end
-
-gnomad_bed = Bio.BED.Reader("/proj/berglab/projects/LiftOver/output/gnomad_v2_sv.sites.hg38.names.v2.bed")
-record = Bio.BED.Record()
-
-while !eof(gnomad_bed)
-	read!(gnomad_bed, record)
-	println(record)
-
-close(gnomad_bed)
